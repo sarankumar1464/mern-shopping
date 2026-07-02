6f4689c2b17c63ffaaa3e12b88c327d13dfdff91
Saran Kumar Kurapati <sarankumarkurapati@gmail.com>
2026-07-02 14:23:58 +0530

    created frontend for my mern app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import {React} from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to MERN Shopping App</h1>
+    </div>
+  )
+}
+
+export default App
